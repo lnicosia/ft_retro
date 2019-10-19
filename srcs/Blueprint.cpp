a38e90d0d9bcd14934e7da88fecacea132b295e6
lnicosia <lnicosia@student.42.fr>
2019-10-19 12:54:48 +0200

    [ADD] Collisions ok? Testing more.

--- srcs/Blueprint.cpp
+++ srcs/Blueprint.cpp
@@ -6,7 +6,7 @@
 /*   By: lnicosia <lnicosia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 13:40:33 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/10/18 21:57:57 by lnicosia         ###   ########.fr       */
+/*   Updated: 2019/10/19 12:49:44 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,32 +74,50 @@ int	Blueprint::parseBlueprint(std::string f)
     std::ifstream       stream;
     std::stringstream   buffer;
     std::string         buff;
+    std::string         tmp;
     int                 fd;
+    size_t              i = 0;
 
+    // Checks if the extension is ".ascii"
     if (f.find_last_of(".ascii") != f.size() - 1)
     {
         std::cerr << "Invalid  file extension" << std::endl;
         return -1;
     }
     stream.open(f);
+    // Prevents from opening symbolic links
     if ((fd = open(f.c_str(), O_NOFOLLOW)) == -1 || close(fd) != 0 || stream.fail())
     {
         std::cerr << "Invalid file" << std::endl;
         return -1;
     }
     std::getline(stream, buff);
+    // Checks if the first line is correct
     if (buff.compare("This is an ascii file!"))
     {
         std::cerr << "Invalid  file" << std::endl;
         return -1;
     }
+    // Get every line in a tmp string and count sizeX and sizeY of our image
     while (std::getline(stream, buff))
     {
-        this->_image += buff + '\n';
-        this->_imageNoN += buff;
+        tmp += buff + '\n';
         this->_sizeY++;
         if (buff.size() > this->_sizeX)
             this->_sizeX = buff.size();
+    }
+    // Copy every line adding missing spaces:
+    //      In _image with '\n'
+    //      In _imageNoN without
+    std::stringstream   sTmp(tmp);
+    while (i < this->_sizeY)
+    {
+        std::getline(sTmp, buff);
+        while (buff.size() < this->_sizeX)
+            buff += ' ';
+        this->_image += buff + '\n';
+        this->_imageNoN += buff;
+        i++;
     }
     std::cerr << f << " image is [" << this->_sizeX << "][" << this->_sizeY << "]" << std::endl;
     return 0;
@@ -111,8 +129,9 @@ void    Blueprint::print(Vec2 pos) const
     std::string         line;
     size_t screenX, screenY;
     getmaxyx(stdscr, screenY, screenX);
-    //mvprintw(0, 0, "Screen size = [%d][%d]", screenX, screenY);
+    mvprintw(0, 0, "My pos = [%d][%d]", pos.getX(), pos.getY());
     size_t  i = 0;
+    size_t  x;
     while (i < this->_sizeY)
     {
         std::getline(ss, line);
@@ -121,16 +140,26 @@ void    Blueprint::print(Vec2 pos) const
         if (pos.getX() < 0)
         {
             line = line.substr(-pos.getX());
-            mvprintw(pos.getY() + i, 0, line.c_str());
+            x = 0;
+            while (x < line.size())
+            {
+                if (line.at(x) != ' ')
+                    mvaddch(pos.getY() + i, x, line.at(x));
+                x++;
+            }
+            //mvprintw(pos.getY() + i, 0, line.c_str());
         }
         else
-            mvprintw(pos.getY() + i, pos.getX(), line.c_str());
-        /*if (pos.getX() + line.size() >= screenX)
-            line = this->_image.substr(i * this->_sizeX, i * this->_sizeX + screenX - pos.getX());
-        else
-            line = this->_image.substr(i * this->_sizeX, i * this->_sizeX + this->_sizeX);
-        if (pos.getX() < 0)
-        mvprintw(pos.getY() + i, )*/
+        {
+            x = 0;
+            while (x < line.size())
+            {
+                if (line.at(x) != ' ')
+                    mvaddch(pos.getY() + i, pos.getX() + x, line.at(x));
+                x++;
+            }
+            //mvprintw(pos.getY() + i, pos.getX(), line.c_str());
+        }
         i++;
     }
 }
